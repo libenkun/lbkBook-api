5e7e44b8047d86f2f74e72f1a1ea1537845d9237
libenkun <1457722982@qq.com>
2020-04-09 17:53:28 +0800

    '.'

---
+++ src/test/java/com/book/lbkbook/LbkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.lbkbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbkbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
